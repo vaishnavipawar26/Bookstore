1de0c0cb874f03a8fb499eae07df0ba4b6fccf28
vaishnavipawar26 <vaishnavipawar.2602@gmail.com>
2026-04-07 19:51:45 +0530

    Update Favorites.js

--- frontend/src/components/Favorites.js
+++ frontend/src/components/Favorites.js
@@ -56,7 +56,7 @@ function Favorites() {
             {/* ⭐ IMAGE ADDED */}
             <img
               style={styles.img}
-              src={`http://localhost:5000/uploads/${b.image}`}
+              src={`https://bookstore-5yhn.onrender.com/uploads/${b.image}`}
               alt={b.title}
             />
 
@@ -74,109 +74,3 @@ function Favorites() {
 }
 
 export default Favorites;
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// function Favorites() {
-//   const [books, setBooks] = useState([]);
-
-//   useEffect(() => {
-//     const fetchFav = async () => {
-//       const token = localStorage.getItem('token');
-
-//       const res = await axios.get(
-//         'http://localhost:5000/api/auth/favorites',
-//         { headers: { Authorization: `Bearer ${token}` } }
-//       );
-
-//       setBooks(res.data);
-//     };
-
-//     fetchFav();
-//   }, []);
-
-//   const styles = {
-//     page: {
-//       padding: '20px'
-//     },
-//     card: {
-//       background: 'white',
-//       padding: '10px',
-//       margin: '10px 0',
-//       borderRadius: '10px',
-//       boxShadow: '0 2px 8px rgba(0,0,0,0.1)'
-//     }
-//   };
-
-//   return (
-//     <div style={styles.page}>
-//       <h2>❤️ Favorites</h2>
-
-//       {books.length === 0 ? (
-//         <p>No favorites yet</p>
-//       ) : (
-//         books.map(b => (
-//           <div key={b._id} style={styles.card}>
-//             <h3>{b.title}</h3>
-//             <p>{b.author}</p>
-//             <p>{b.category}</p>
-//           </div>
-//         ))
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Favorites;
-
-
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// function Favorites(){
-
-//   const [books, setBooks] = useState([]);
-
-//   useEffect(()=>{
-//     const fetchFav = async ()=>{
-//       const token = localStorage.getItem('token');
-
-//       const res = await axios.get(
-//         'http://localhost:5000/api/auth/favorites',
-//         {
-//           headers: { Authorization: `Bearer ${token}` }
-//         }
-//       );
-
-//       setBooks(res.data);
-//     };
-
-//     fetchFav();
-//   },[]);
-
-//   return (
-//     <div>
-//       <h2>❤️ My Favorite Books</h2>
-
-//       {books.length === 0 ? (
-//         <p>No favorites yet</p>
-//       ) : (
-//         books.map(book => (
-//           <div key={book._id}>
-//             <h3>{book.title}</h3>
-//             <p>{book.author}</p>
-//             <p>{book.category}</p>
-//           </div>
-//         ))
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Favorites;
