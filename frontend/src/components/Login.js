06ede01831746711098813243c02f1dbd85a77aa
vaishnavipawar26 <vaishnavipawar.2602@gmail.com>
2026-04-07 19:52:11 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,35 +1,3 @@
-// import React, {useState} from 'react';
-// import axios from 'axios';
-
-// function Login({setUser}) {
-//   const [data,setData] = useState({email:'', password:''});
-
-//   const handleChange = (e)=>{
-//     setData({...data,[e.target.name]:e.target.value});
-//   }
-
-//   const handleLogin = async ()=>{
-//     const res = await axios.post('http://localhost:5000/api/auth/login', data);
-
-//     // save token + role
-//     localStorage.setItem('token', res.data.token);
-//     localStorage.setItem('role', res.data.role);
-
-//     setUser(res.data.role);
-//   }
-
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <input name="email" placeholder="Email" onChange={handleChange}/>
-//       <input name="password" placeholder="Password" onChange={handleChange}/>
-//       <button onClick={handleLogin}>Login</button>
-//     </div>
-//   );
-// }
-// export default Login;
-
-
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
